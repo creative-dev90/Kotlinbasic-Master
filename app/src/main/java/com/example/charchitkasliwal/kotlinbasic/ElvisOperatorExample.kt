0bb5fd6f1e9096b66507b4fc72e13df913b6a5aa
charchit.kasliwal <kasliwalcharchit@gmail.com>
2017-06-08 15:40:34 +0530

    Elvis Operator

---
+++ app/src/main/java/com/example/charchitkasliwal/kotlinbasic/ElvisOperatorExample.kt
@@ -0,0 +1,34 @@
+package com.example.charchitkasliwal.kotlinbasic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ElvisOperatorExample : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elvis_operator)
+        elvisOperator("")
+    }
+
+    private fun elvisOperator(b : String) {
+        if (b != null && b.length > 0) {
+            print("String of length ${b.length}")
+        } else {
+            print("Empty string")
+        }
+
+        // Safe Call Operator ?.
+        val b1 = b?.length
+
+        val l: Int = if (b != null) b.length else -1
+
+        val l1 = b?.length ?: -1     // ?: Elvis Operator
+        // If the expression to the left of ?: is not null,
+        // the elvis operator returns it, otherwise it returns the expression to the right.
+        // Note that the right-hand side expression is evaluated only if the left-hand side is null.
+
+
+       // val l = b?.length ?: -1
+    }
+}
